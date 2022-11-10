1deb5256c33af20f2e45997e2ba57920a66c8a95
reeeeflex <rafaelpastora@yahoo.com>
2022-11-10 13:28:49 -0500

    add clock to web

--- app.js
+++ app.js
@@ -18,3 +18,34 @@ window.addEventListener('scroll', () => {
   hamburger.classList.remove('active');
   navLinks.classList.remove('active');
 });
+
+function updateClock() {
+  let now = new Date(), // current date
+    months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+  (time =
+    now.getHours() - 12 + ':' + now.getMinutes() + ':' + now.getSeconds()),
+    // a cleaner way than string concatenation
+    (date = [now.getDate(), months[now.getMonth()], now.getFullYear()].join(
+      ' '
+    ));
+
+  // set the content of the element with the ID time to the formatted string
+  document.getElementById('time').innerHTML = [date, time].join(' - ');
+
+  // call this function again in 1000ms (every second)
+  setTimeout(updateClock, 1000);
+}
+updateClock(); // initial call
