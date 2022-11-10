f68d5bdecd271ac0c389c09ae5f79bc2a1f9d1a4
reeeeflex <rafaelpastora@yahoo.com>
2022-11-09 22:48:37 -0500

    add hamburger js

---
+++ app.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav__links');
+
+// sets active class to css allowing for animations
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav__link').forEach(e =>
+  e.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navLinks.classList.remove('active');
+  })
+);
